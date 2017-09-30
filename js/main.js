aeeb67d7af6e7aec3c7dc78999434668e395cf6d
zorbax <otto94@gmail.com>
2017-09-30 14:05:39 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 ---
 layout: null
+sitemap:
+  exclude: 'yes'
 ---
+
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
@@ -17,16 +20,8 @@ $(document).ready(function () {
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-  if (window.location.hash && window.location.hash == '#about') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-  if (window.location.hash && window.location.hash == '#services') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
 
-
-
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
