29207fd3bbf60e627ed95d141223fc00e7384cf5
Otoniel Maya <otto94@gmail.com>
2024-06-13 22:57:02 +0200

    minor fix

--- js/main.js
+++ js/main.js
@@ -21,12 +21,12 @@ $(document).ready(function () {
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list fa-circle-xmark animated fadeIn')
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list fa-circle-xmark animated fadeIn')
   })
 
 })
